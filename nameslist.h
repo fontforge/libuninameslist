9fe98ca81b0148f7bf82a4c2858b13939862398b
George Williams <pfaedit@users.sourceforge.net>
2003-07-11 05:12:35 +0000

    Upgrade to Unicode 4.0

--- nameslist.h
+++ nameslist.h
@@ -10,10 +10,10 @@ struct unicode_nameannot {
     const char *name, *annot;
 };
 
-extern const struct unicode_block UnicodeBlock[124];
+extern const struct unicode_block UnicodeBlock[139];
 
 #define UNICODE_NAME_MAX	94
-#define UNICODE_ANNOT_MAX	372
+#define UNICODE_ANNOT_MAX	371
 extern const struct unicode_nameannot * const *const UnicodeNameAnnot[];
 
 /* Index by: UnicodeNameAnnot[(uni>>16)&0x1f][(uni>>8)&0xff][uni&0xff] */
