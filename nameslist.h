ce1d8f57e905a23190a449479194e5eae1cc6cf8
George Williams <pfaedit@users.sourceforge.net>
2009-12-31 03:22:55 +0000

    Update to version 5.2. (French translation still at 5.0)

--- nameslist.h
+++ nameslist.h
@@ -10,10 +10,10 @@ struct unicode_nameannot {
     const char *name, *annot;
 };
 
-extern const struct unicode_block UnicodeBlock[185];
+extern const struct unicode_block UnicodeBlock[211];
 
-#define UNICODE_NAME_MAX	94
-#define UNICODE_ANNOT_MAX	560
+#define UNICODE_NAME_MAX	83
+#define UNICODE_ANNOT_MAX	462
 extern const struct unicode_nameannot * const *const UnicodeNameAnnot[];
 
 /* Index by: UnicodeNameAnnot[(uni>>16)&0x1f][(uni>>8)&0xff][uni&0xff] */
