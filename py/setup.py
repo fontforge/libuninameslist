9fef08cc04ebf77ffc0230a785d8d86f110a4e34
Joe Da Silva <digital@joescat.com>
2017-11-14 00:03:39 -0800

    Make uninameslist.py install or uninstall
    Python doesn't follow same convention used by autoconf so install goes
    into /usr/lib/python... instead of expected /usr/local/lib/python....

---
+++ py/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='uninameslist',
+    version='0.1',
+    author="Shriramana Sharma",
+    license="BSD-3",
+    description='Python wrapper for LibUniNamesList',
+    url='https://github.com/fontforge/libuninameslist/releases/',
+    py_modules=['uninameslist']
+    )
+
